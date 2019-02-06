56e52b2f9fa146300549bfa7b6a3ac5c38f7d937
Anna Karutina <anna.karutina@khk.ee>
2019-02-06 10:38:17 +0200

    oop näide - klassi loomine ja muutujate defineerimine ning objekti loomine

---
+++ oopExample/Text.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 06.02.2019
+ * Time: 10:30
+ */
+
+class Text
+{
+    var $str = '';
+}
---
+++ oopExample/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 06.02.2019
+ * Time: 10:35
+ */
+require_once 'Text.php';
+
+$hello1 = new Text();
+
+echo '<pre>';
+print_r($hello1);
+echo '</pre>';
