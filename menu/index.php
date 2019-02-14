0421429702c82e10591197872cb3afd2d8dbfa6b
Anna Karutina <anna.karutina@khk.ee>
2019-02-14 11:34:47 +0200

    oop menu - loodud menüü lehe sisu template, mis pannakse põhitemplate sisse

--- menu/index.php
+++ menu/index.php
@@ -1,10 +1,13 @@
 <?php
 require_once 'conf.php';
-
+// loome vajalikud vaade objektid
 $mainTmpl = new Template('main');
+$contentTmpl = new Template('content');
 
 $mainTmpl->set('title', 'Menu App');
-$mainTmpl->set('content', 'Menu App Content');
+
+
+$mainTmpl->set('content', $contentTmpl->parse());
 
 echo $mainTmpl->parse();
 
